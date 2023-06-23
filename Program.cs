9056a7a1db33710a0864fbfbfd80bb5e958ef14c
MokhovEa <137217487+MokhovEa@users.noreply.github.com>
2023-06-23 19:31:34 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,14 @@
-﻿Console.Clear();
- int i = 1;
-            bool not = true;
-
-            Console.WriteLine("Введите число:");
-            int num = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Чётные числа от 1 до " + num);
-            while (i <= num)
-            {
-                if (i % 2 != 1)
-                {
-                    Console.Write(i + ", ");
-                    not = false;
-                }
-                i++;
-            }
-
-            if (not)
-            {
-                Console.WriteLine("Нет чётных чисел!");
-            }
+﻿Console.Clear();            //Очистка экрана
+Console.WriteLine("Введите первое число => "); // вывод приглашения для пользователя
+string number1 = Console.ReadLine(); //Ввод пользователя
+Console.WriteLine("Введите второе число => "); //преобразует значение в целое число
+string number2 = Console.ReadLine(); //Ввод пользователя
+int num1 = Convert.ToInt32(number1);
+int num2 = Convert.ToInt32(number2);
+if (num1 > num2)
+{
+    Console.WriteLine($"Число {number1} большее, числа {number2}");
+}
+else {
+    Console.WriteLine($"Число {number2} большее, числа {number1}");
+}
